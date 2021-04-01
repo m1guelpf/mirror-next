0e182da1d4e8784033f61312ebaf87a8e29edd7d
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-04-02 01:01:12 +0200

    wip

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
+require('./scripts/resolve-ens')()
+
 module.exports = {
 	future: {
 		webpack5: true,
 	},
-	async rewrites() {
-		return [
-			{
-				source: '/feed.xml',
-				destination: '/api/feed',
-			},
-		]
-	},
-	exportPathMap() {
-		const resolveEns = require('./scripts/resolve-ens')
-
-		resolveEns()
-	},
+	rewrites: async () => [{ source: '/feed.xml', destination: '/api/feed' }],
 }
