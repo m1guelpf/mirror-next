8d95fdc1788ad26bb433375c9628a6e885b773f6
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-04-03 00:50:54 +0200

    Retry ENS resolve on startup

--- scripts/resolve-ens.js
+++ scripts/resolve-ens.js
@@ -3,7 +3,11 @@ const fs = require('fs')
 const { ethers } = require('ethers')
 
 module.exports = () => {
-	if (fs.existsSync('./src/data/ens.js')) return
+	if (fs.existsSync('./src/data/ens.js')) {
+		// If the Mirror subdomain wasn't set on the first run, we might have an invalid publication address.
+		// This makes sure invalid addresses are re-calculated on every run instead of just the first one.
+		if (!String(fs.readFileSync('./src/data/ens.js')).includes("'null'")) return
+	}
 
 	const provider = new ethers.providers.InfuraProvider(null, process.env.INFURA_ID)
 
