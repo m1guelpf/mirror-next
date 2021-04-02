ab0e113e4cd88a3d0d17f76e464827f97b87cf56
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-04-02 02:07:27 +0200

    Use preact on prod

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ module.exports = {
 	future: {
 		webpack5: true,
 	},
+	webpack: (config, { dev, isServer }) => {
+		if (!dev && !isServer) {
+			Object.assign(config.resolve.alias, {
+				react: 'preact/compat',
+				'react-dom': 'preact/compat',
+			})
+		}
+
+		return config
+	},
 	rewrites: async () => [
 		{ source: '/feed.xml', destination: '/api/feed' },
 		{ source: '/post/:slug', destination: '/api/post' },
