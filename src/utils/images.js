ac27f3a4026214a98fa86ecc38941cd2d45af757
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-04-06 10:48:00 +0200

    fix for large images

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,5 +1,12 @@
-import requestImageSize from 'request-image-size'
+import axios from 'axios'
+import sizeOf from 'image-size'
 
 export const calculateSizes = async markdown => {
-	return Object.fromEntries(await Promise.all([...markdown.matchAll(/!\[[^\]]*\]\((?<url>.*?)\s*("(?:.*[^"])")?\s*\)/gm)].map(async match => [match.groups.url, await requestImageSize(match.groups.url)])))
+	return Object.fromEntries(await Promise.all([...markdown.matchAll(/!\[[^\]]*\]\((?<url>.*?)\s*("(?:.*[^"])")?\s*\)/gm)].map(async match => [match.groups.url, await calculateImageSize(match.groups.url)])))
+}
+
+export const calculateImageSize = async url => {
+	const image = await axios.get(url, { responseType: 'arraybuffer' }).then(res => Buffer.from(res.data, 'binary'))
+
+	return sizeOf(image)
 }
