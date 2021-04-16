3c30f7b7b5b07b4489e6a7c2b20f7045a4dce380
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-04-16 18:51:15 +0200

    Improve excerpt rendering

--- src/utils/excerpt.js
+++ src/utils/excerpt.js
@@ -1,10 +1,14 @@
 export const getExcerpt = content => {
-	const firstParagraphs = content.split('\n\n').slice(0, 4)
+	let firstParagraphs = content.split('\n\n').slice(0, 4)
 	let separatorIndex = null
 
 	firstParagraphs.forEach((p, i) => {
 		if (p === '---') separatorIndex = i
 	})
 
-	return firstParagraphs.slice(0, separatorIndex ? separatorIndex : 4).join('\n\n')
+	firstParagraphs = firstParagraphs.slice(0, separatorIndex ? separatorIndex : 4)
+
+	if (firstParagraphs[firstParagraphs.length - 1].startsWith('#')) firstParagraphs.pop()
+
+	return firstParagraphs.join('\n\n')
 }
