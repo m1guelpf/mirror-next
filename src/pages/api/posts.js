b41854300eb6ce4e0ccce692eef782cb8947fc37
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-07-16 05:06:13 +0200

    Add URL to posts endpoint

--- src/pages/api/posts.js
+++ src/pages/api/posts.js
@@ -1,7 +1,7 @@
 import { getEntries } from '@/data/entries'
 
-export default async (_, res) => {
+export default async ({ headers: { host } }, res) => {
 	res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
 
-	return res.json(await getEntries())
+	return res.json((await getEntries()).map(entry => ({ ...entry, url: `https://${host}/post/${entry.slug}` })))
 }
