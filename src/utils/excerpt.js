f36d5a2af6b2c99fddb6747eec01ad38a340cefe
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-08-26 00:57:14 +0200

    Remove excerpt limiting

--- src/utils/excerpt.js
+++ src/utils/excerpt.js
@@ -1,12 +1,5 @@
 export const getExcerpt = content => {
 	let firstParagraphs = content.split('\n\n').slice(0, 4)
-	let separatorIndex = null
-
-	firstParagraphs.forEach((p, i) => {
-		if (p === '---') separatorIndex = i
-	})
-
-	firstParagraphs = firstParagraphs.slice(0, separatorIndex ? separatorIndex : 4)
 
 	if (firstParagraphs[firstParagraphs.length - 1].startsWith('#')) firstParagraphs.pop()
 
