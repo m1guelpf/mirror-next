bb486dc8ead2d8abf226a37aceb2e2a490839733
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-03-14 06:02:11 +0100

    wip

--- src/pages/500.js
+++ src/pages/500.js
@@ -1,9 +1,9 @@
 import { getPublication } from '@/data/publication'
 import Link from 'next/link'
 
-const PageNotFound = () => (
+const InternalError = () => (
 	<div className="absolute inset-0 flex flex-col space-y-10 items-center justify-center h-full">
-		<p className="text-gray-800 text-3xl dark:text-gray-300 font-semibold">Page Not Found</p>
+		<p className="text-gray-800 text-3xl dark:text-gray-300 font-semibold">Internal Error</p>
 		<Link href="/">
 			<a className="bg-blue-100 dark:bg-fuchsia-400 dark:bg-opacity-20 font-medium text-blue-500 dark:text-fuchsia-300 rounded-lg px-4 py-3 hover:ring-4 ring-blue-50 dark:ring-fuchsia-400 dark:ring-opacity-20 transition duration-300 text-base shadow-xs hover:shadow-xs sm:text-lg">Return Home</a>
 		</Link>
@@ -16,4 +16,4 @@ export async function getStaticProps() {
 	return { props: { publication } }
 }
 
-export default PageNotFound
+export default InternalError
